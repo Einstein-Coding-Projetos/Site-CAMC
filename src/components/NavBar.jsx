d50b1188168f595e5b57d683fb6c19370e65478c
lelegds <helenaduartesantos29@gmail.com>
2026-02-05 12:14:04 -0300

    Atualiza Home e Navbar e adiciona imagem da Marie Curie

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,24 +4,25 @@ import "../css/NavBar.css";
 function NavBar() {
   return (
     <nav className="navbar">
-      <div className="navbar-brand">
-        {/* ALTERAÇÃO AQUI: Mudando o texto do projeto para o novo nome */}
-        <Link to="/">Site CAMC</Link>
-      </div>
-      <div className="navbar-links">
-        {/* Estes links já estão corretos e correspondem ao App.jsx */}
-        <Link to="/" className="nav-link">
-          Home
-        </Link>
-        <Link to="/produtos" className="nav-link">
-          Produtos
-        </Link>
-        <Link to="/gestao" className="nav-link">
-          Gestão
-        </Link>
-        <Link to="/eventos" className="nav-link">
-          Eventos
-        </Link>
+      <div className="navbar-container">
+        <div className="navbar-brand">
+          <Link to="/">CAMC</Link>
+        </div>
+
+        <div className="navbar-links">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+          <Link to="/produtos" className="nav-link">
+            Produtos
+          </Link>
+          <Link to="/gestao" className="nav-link">
+            Gestão
+          </Link>
+          <Link to="/eventos" className="nav-link">
+            Eventos
+          </Link>
+        </div>
       </div>
     </nav>
   );
